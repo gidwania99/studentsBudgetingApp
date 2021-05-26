8fc202007cefb1d653d8226a66c5fb1b22233f64
Willy Macharia <willymash@gmail.com>
2021-05-26 05:49:56 +0300

    Added student budgeting app as the solved error version of personal budgeting app

---
+++ StudentsBudgetingApp/app/src/main/java/com/willteksoftwares/studentsbudgetingapp/Activities/MonthSpendingActivity.java
@@ -0,0 +1,16 @@
+package com.willteksoftwares.studentsbudgetingapp.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.willteksoftwares.studentsbudgetingapp.R;
+
+public class MonthSpendingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_month_spending);
+    }
+}
